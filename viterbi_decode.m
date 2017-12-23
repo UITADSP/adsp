76bac878e72addd0c2b013a6dce9d7b6e79db9e7
Ta Xuan Truong <truongtx8@gmail.com>
2017-12-23 23:28:50 +0700

    Update Viterbi decoder, still not work ;)

--- viterbi_decode.m
+++ viterbi_decode.m
@@ -13,106 +13,21 @@ function guess_output_bits = viterbi_decode(input_bits, type)
         viterbi_path(1,:) = [0 0 0 0 0]; %[weight, decoded_index, decoded_bit, rx_bit0, rx_bit1]
         
         for bit = 1:2:length(input_bits)
-            if previous_stage == [0 0]
+            
+            output_bit = is_valid_transition(previous_stage, input_bits(bit:bit+1))
+            
+            if output_bit ~= -1 % valid transition
+                viterbi_row = viterbi_row + 1;
+                viterbi_path(viterbi_row,:) = [0 (bit+1)/2 output_bit previous_stage]
+                
                 previous_stage = input_bits(bit:bit+1);
+            else % invalid transition
+                viterbi_row = viterbi_row + 1;
+                viterbi_path(viterbi_row,:) = [1 (bit+1)/2 output_bit -1 -1]
                 
-                if input_bits(bit:bit+1) == [0 0]
-                    
-                    viterbi_row = viterbi_row + 1;
-                    viterbi_path(viterbi_row,:) = [0 (bit+1)/2 0 0 0]
-                    
-                elseif input_bits(bit:bit+1) == [1 1]
-                    
-                    viterbi_row = viterbi_row + 1;
-                    viterbi_path(viterbi_row,:) = [0 (bit+1)/2 1 0 0]
-                    
-                elseif input_bits(bit:bit+1) == [1 0]
-                    % Correct pre-bits must be 1 0 or 1 1
-                    
-                    viterbi_row = viterbi_row + 1;
-                    viterbi_path(viterbi_row,:) = [1 (bit+1)/2 0 1 1]
-                    
-                    viterbi_row = viterbi_row + 1;
-                    viterbi_path(viterbi_row,:) = [1 (bit+1)/2 1 1 0]
-                    
-                elseif input_bits(bit:bit+1) == [0 1]
-                    % Correct pre-bits must be 1 0 or 1 1
-                    
-                    viterbi_row = viterbi_row + 1;
-                    viterbi_path(viterbi_row,:) = [1 (bit+1)/2 0 1 0]
-                    
-                    viterbi_row = viterbi_row + 1;
-                    viterbi_path(viterbi_row,:) = [1 (bit+1)/2 1 1 1]
-                end
+                % trying to guess the correct pattern
                 
-            elseif previous_stage == [0 1]
-                previous_stage = input_bits(bit:bit+1);
-                
-                if input_bits(bit:bit+1) == [0 0]
-                    viterbi_row = viterbi_row + 1;
-                    viterbi_path(viterbi_row,:) = [0 (bit+1)/2 1 0 1]
-                    
-                elseif input_bits(bit:bit+1) == [1 1]
-                    viterbi_row = viterbi_row + 1;
-                    viterbi_path(viterbi_row,:) = [0 (bit+1)/2 0 0 1]
-                    
-                else
-                    
-                    viterbi_row = viterbi_row + 1;
-                    viterbi_path(viterbi_row,:) = [1 (bit+1)/2 0 0 1]
-                    
-                    viterbi_row = viterbi_row + 1;
-                    viterbi_path(viterbi_row,:) = [1 (bit+1)/2 1 0 1]
-                end
-                
-            elseif previous_stage == [1 0]
-                previous_stage = input_bits(bit:bit+1);
-                
-                if input_bits(bit:bit+1) == [0 1]
-                    viterbi_row = viterbi_row + 1;
-                    viterbi_path(viterbi_row,:) = [0 (bit+1)/2 0 1 0]
-                    
-                elseif input_bits(bit:bit+1) == [1 0]
-                    viterbi_row = viterbi_row + 1;
-                    viterbi_path(viterbi_row,:) = [0 (bit+1)/2 1 1 0]
-                
-                elseif input_bits(bit:bit+1) == [0 0]
-                    % Correct pre-bits must be 0 0 or 0 1
-                    
-                    viterbi_row = viterbi_row + 1;
-                    viterbi_path(viterbi_row,:) = [1 (bit+1)/2 0 0 0]
-                    
-                    viterbi_row = viterbi_row + 1;
-                    viterbi_path(viterbi_row,:) = [1 (bit+1)/2 1 0 1]
-                
-                elseif input_bits(bit:bit+1) == [1 1]
-                    % Correct pre-bits must be 0 0 or 0 1
-                    
-                    viterbi_row = viterbi_row + 1;
-                    viterbi_path(viterbi_row,:) = [1 (bit+1)/2 0 0 1]
-                    
-                    viterbi_row = viterbi_row + 1;
-                    viterbi_path(viterbi_row,:) = [1 (bit+1)/2 1 0 0]
-                end
-                
-            elseif previous_stage == [1 1]
-                previous_stage = input_bits(bit:bit+1)
-                
-                if input_bits(bit:bit+1) == [0 1]
-                    viterbi_row = viterbi_row + 1;
-                    viterbi_path(viterbi_row,:) = [0 (bit+1)/2 1 1 1]
-                elseif input_bits(bit:bit+1) == [1 0]
-                    viterbi_row = viterbi_row + 1;
-                    viterbi_path(viterbi_row,:) = [0 (bit+1)/2 0 1 1]
-                else
-                    
-                    viterbi_row = viterbi_row + 1;
-                    viterbi_path(viterbi_row,:) = [1 (bit+1)/2 0 1 1]
-                    
-                    viterbi_row = viterbi_row + 1;
-                    viterbi_path(viterbi_row,:) = [1 (bit+1)/2 1 1 1]
-                end
-
+                %previous_stage = input_bits(bit:bit+1);
             end
 
         end
@@ -168,5 +83,38 @@ function output_bit = is_valid_transition(pre_bits, current_bits)
         else
             output_bit = -1;
         end
+    end
+end
+
+function output_bit = guess_valid_transition(pre_bits, decoded_bit)
+    % returns correct current bits from previous bits
+    if pre_bits == [0 0]
+        if decoded_bit == 0
+        elseif decoded_bit == 1
+        else
+            return
+        end
+        
+    elseif pre_bits == [0 1]
+        if decoded_bit == 0
+        elseif decoded_bit == 1
+        else
+            return
+        end
+        
+    elseif pre_bits == [1 0]
+        if decoded_bit == 0
+        elseif decoded_bit == 1
+        else
+            return
+        end
+        
+    elseif pre_bits == [1 1]
+        if decoded_bit == 0
+        elseif decoded_bit == 1
+        else
+            return
+        end
+
     end
 end
